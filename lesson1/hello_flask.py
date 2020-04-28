a8603da4ad9c52df3de4e8e9d569d53defdb2b21
dubiZA <iamdzh@outlook.com>
2020-04-27 21:25:13 -0600

    Connect to DB with SQLAlchemy and create table

--- lesson1/hello_flask.py
+++ lesson1/hello_flask.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todoapp_development'
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+
+db.create_all()
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
