3ee8a17cb702200b194dd2f45dc32cb4c6eb93d5
dubiZA <iamdzh@outlook.com>
2020-04-28 05:55:33 -0600

    Move hello_flask to lesson3 dir and SQL stuff

--- lesson3/hello_flask.py
+++ lesson3/hello_flask.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todoapp_development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -12,12 +13,15 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
+    def __repr__(self):
+        return f'<Person ID: {self.id}, Name: {self.name}>'
 
 db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    person = Person.query.first()
+    return f'Hello, {person.name}!'
 
 
 if __name__ == '__main__':
