4400e1bef2c2f388ab7b75822fb2b504fde0127a
dubiZA <iamdzh@outlook.com>
2020-05-04 06:46:43 -0600

    App error handling

--- lesson5/app.py
+++ lesson5/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///todoapp'
@@ -25,14 +25,22 @@ def index():
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todos(description=description)
-    db.session.add(todo)
-    db.session.commit()
-
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todos(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
 
 
 if __name__ == '__main__':
